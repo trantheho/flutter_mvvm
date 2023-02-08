8c55f373a7c41e23ce42dc1f5ba119b578bd20fe
Tran The Ho <tran.theho96@gmail.com>
2023-02-08 18:21:16 +0700

    remove unused code

--- lib/core/manager/theme_manager.dart
+++ lib/core/manager/theme_manager.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mvvm/core/utils/styles.dart';
-import 'package:flutter_mvvm/data/datasources/local/local_data_source.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../data/datasources/local/hive_storage.dart';
 
 class ThemeManager {
   static final ThemeData light = ThemeData(
@@ -54,32 +50,3 @@ class ThemeManager {
     ),
   );
 }
-
-/*
-final themeInit = FutureProvider<ThemeMode>((ref) async => await HiveStorage.instance.getTheme());
-
-final themeProvider = StateNotifierProvider<ThemeProvider, ThemeMode>((ref){
-
-  //final theme = ref.watch(themeInit).value!;
-
-  return ThemeProvider(ThemeMode.light, HiveStorage.instance);
-});
-
-
-class ThemeProvider extends StateNotifier<ThemeMode>{
-  final ThemeMode initThemeMode;
-  final LocalDataSource localDataSource;
-
-  ThemeProvider(this.initThemeMode, this.localDataSource)  : super(initThemeMode);
-
-
-  Future<void> updateTheme(ThemeMode themeMode) async {
-    try{
-      await localDataSource.updateTheme(themeMode.name);
-      state = themeMode;
-    }
-    catch (e){
-      throw StateError("update theme failed with ${e.toString()}");
-    }
-  }
-}*/
--- lib/core/utils/app_validation.dart
+++ lib/core/utils/app_validation.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_mvvm/core/utils/app_utils.dart';
+
 
 enum EmailValidationState {
   valid,
@@ -137,68 +137,6 @@ mixin Validator {
       }
 
       final regex = RegExp(_passwordRegex);
-      if (!regex.hasMatch(password)) {
-        return PasswordValidationState.badPassword;
-      }
-    } else {
-      return PasswordValidationState.nonPassword;
-    }
-
-    return PasswordValidationState.valid;
-  }
-}
-
-
-
-class _Validator {
-  final emailRegex =
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  final passwordRegex =
-      r"^([ a-zA-ZĂ€ĂĂ‚ĂƒĂˆĂ‰ĂĂŒĂĂ’Ă“Ă”Ă•Ă™ĂĂĂ Ă¡Ă¢Ă£Ă¨Ă©ĂªĂ¬Ă­Ă²Ă³Ă´ĂµĂ¹ĂºĂ½Ä‚ÄƒÄÄ‘Ä¨Ä©Å¨Å©Æ Æ¡Æ¯Æ°áº -á»¹]+(([',. -][a-zA-Z ])?[a-zA-ZĂ€ĂĂ‚ĂƒĂˆĂ‰ĂĂŒĂĂ’Ă“Ă”Ă•Ă™ĂĂĂ Ă¡Ă¢Ă£Ă¨Ă©ĂªĂ¬Ă­Ă²Ă³Ă´ĂµĂ¹ĂºĂ½Ä‚ÄƒÄÄ‘Ä¨Ä©Å¨Å©Æ Æ¡Æ¯Æ°áº -á»¹]*)*)$";
-
-  EmailValidationState validateEmail(String? email) {
-    if (email != null && email.isNotEmpty) {
-      final regex = RegExp(emailRegex);
-      if (!regex.hasMatch(email)) {
-        return EmailValidationState.badEmail;
-      }
-    } else {
-      return EmailValidationState.nonEmail;
-    }
-
-    return EmailValidationState.valid;
-  }
-
-  PhoneValidationState validatePhoneNumber(String? phone) {
-    if (phone == null) {
-      return PhoneValidationState.badPhoneNumber;
-    } else if (phone.isEmpty) {
-      return PhoneValidationState.badPhoneNumber;
-    } else if (phone.length < 9) {
-      return PhoneValidationState.badPhoneNumber;
-    }
-
-    return PhoneValidationState.valid;
-  }
-
-  PasswordValidationState validateName(String? stringName, int length) {
-    if (stringName == null || stringName.isEmpty || stringName == '') {
-      return PasswordValidationState.badPassword;
-    } else if (stringName.length > length) {
-      return PasswordValidationState.badPassword;
-    } else if (!RegExp(passwordRegex).hasMatch(stringName)) {
-      return PasswordValidationState.badPassword;
-    }
-    return PasswordValidationState.valid;
-  }
-
-  PasswordValidationState validatePassword(String? password) {
-    if (password != null && password.isNotEmpty) {
-      if (password.length < 8) {
-        return PasswordValidationState.badLength;
-      }
-
-      final regex = RegExp(passwordRegex);
       if (!regex.hasMatch(password)) {
         return PasswordValidationState.badPassword;
       }
